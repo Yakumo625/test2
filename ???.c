805f861e0e2944f2a38bcdd1a9fbf9a404bc425e
Yakumo625 <1325456328@qq.com>
2021-01-19 03:19:22 -0800

    third commit

---
+++ ???.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main ()
+{
+    int row=0,rank=0,num=0,blank=0;//row行，rank列变量,blank变量控制打印空格
+	int n[10];
+	for(num=1;num<=10;num++)//给数组赋初值，通过修改这一步得到你想要的数据
+	{
+	    n[num-1]=num;//通过修改这一步得到你想要的数据，例如2*num-1
+	}
+ 
+	num=0;
+ 
+	for(row=1;row<=4;row++)//输出
+	{
+	    for(blank=1;blank<=5-row;blank++)
+		   printf(" ");
+		   
+	        for(rank=1;rank<=row;rank++)
+		{
+		   printf("%d  ",n[num]);
+		     num++;
+		   if(rank==row)
+		      printf("\n");
+		}
+	}
+	return 0;
+}
